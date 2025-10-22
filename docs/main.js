b9a4508cb678610961696ae08a32888b2cd565e0
Ameya <ameyakool3@yahoo.com>
2025-10-23 01:27:34 +0530

    Updated for optimization

--- docs/main.js
+++ docs/main.js
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.remove("opacity-0", "translate-y-8");
+        entry.target.classList.remove("opacity-0", "translate-y-10");
         entry.target.classList.add("opacity-100", "translate-y-0");
         observer.unobserve(entry.target);
       }
